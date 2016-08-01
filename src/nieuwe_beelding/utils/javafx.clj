a62cd48c325d45c22e2e93c66ac703f6d75036f1
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-08-01 09:59:19 +0300

    Event handlers

--- src/nieuwe_beelding/utils/javafx.clj
+++ src/nieuwe_beelding/utils/javafx.clj
@@ -12,12 +12,22 @@
    javafx.scene.layout.Region
    javafx.scene.paint.Color
    javafx.scene.shape.Arc
+   javafx.event.EventHandler
+   javafx.stage.WindowEvent
    javafx.scene.shape.Line)
   (:gen-class
    :name nieuwe-beelding.utils.javafx.Stage
    :extends javafx.application.Application
    :methods [^:static [getStage [] clojure.lang.Atom]]
    :main true))
+
+(defn event-handler*
+  [f]
+  (reify javafx.event.EventHandler
+    (handle [this e] (f e))))
+
+(defmacro event-handler [arg & body]
+  `(event-handler* (fn ~arg ~@body)))
 
 (defn display [scene]
   (let []
@@ -41,7 +51,9 @@
 
 (defn -start [this stage]
   (Platform/setImplicitExit false)
+  (.addEventHandler stage WindowEvent/ANY (event-handler [e] (println e)))
   (reset! primary-stage stage))
 
 ;; (defn -main [& args]
 ;;   (Application/launch nieuwe-beelding.utils.javafx.Stage args))
+
